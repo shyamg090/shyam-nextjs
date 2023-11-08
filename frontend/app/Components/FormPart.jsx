4d97701e9d45aea38ab054f7ed462d361bbfcd28
shyamg090 <samshyam9630@gmail.com>
2023-11-08 18:43:55 +0530

    small modifications

--- frontend/app/Components/FormPart.jsx
+++ frontend/app/Components/FormPart.jsx
@@ -42,8 +42,11 @@ export default function FormPart() {
           <h3 className='text-2xl md:text-3-xl text-center mt-10 py-4 '>
               <span className='border-b-4 border-b-teal-600 py-2'>Submit Your Message</span>
           </h3>
-          <div className='flex items-center justify-center '>
-              {alert ? <div className='m-2 flex items-center justify-center bg-transparent bg-cyan-400 w-1/4 h-10  rounded-lg' > <h1 className='text-white text-2xl' >{status}</h1> </div> : ""}
+          <div className='fixed z-20 top-5 left-1 w-1/2 md:w-1/4 h-16 rounded-xl'>
+              {alert ? <div class="flex justify-between py-4 px-8 w-full h-full bg-[#98f5e1] rounded-xl  text-[#236c5b]">
+                  <p class="font-sans">{status}</p>
+                  {/* <button class="font-bold">&#10005;</button> */}
+              </div> : ""}
           </div>
 
           <form onSubmit={submitForm} className='flex flex-col justify-center items-center my-10' >
--- frontend/app/Components/Projects.jsx
+++ frontend/app/Components/Projects.jsx
@@ -16,7 +16,8 @@ export default function Projects() {
       id: 1,
       image: techtune,
       name: "TechTune Motors",
-      desc: "During my internship, I was tasked to build a MERN Stack website for Techtune Motors",
+      desc: "During my internship, I developed a dynamic MERN Stack website for Techtune Motors. The site highlights their services, prices, and features a user-friendly form for requesting callbacks. It efficiently showcases the company's offerings, ensuring a seamless user experience for visitors seeking information and assistance.",
+      skills : ["React.js", "MongoDB", "Express.js", "Node.js", "TailwindCSS"],
       link: "https://techtunemotors.netlify.app/",
       github: "https://github.com/shyamg090/TechTuneMotors"
     },
@@ -24,21 +25,24 @@ export default function Projects() {
       id: 2,
       image: moodmovie,
       name: "MoodMovies",
-      desc: "Discover trending films, explore cast and crew, and personalize your cinematic journey effortlessly.",
+      desc: "Explore the cinematic world effortlessly with our platform, featuring top trending movies and TV shows, along with top IMDB rated shows. Discover detailed descriptions of films and shows, delve into the cast and crew information, and personalize your movie journey. Stay updated with the latest trends and enjoy a seamless entertainment experience.",
+      skills: ["React.js", "SCSS", "JavaScript","TMDB API"],
       link: "https://moodmovie.netlify.app/",
       github: "https://github.com/shyamg090/MoodFlix"
     }, {
       id: 3,
       image: todo,
       name: "Todo App",
-      desc: "Efficient task storage app crafted with React.js & SCSS for seamless task management",
+      desc: "Efficient task storage app crafted for seamless task management",
+      skills: ["React.js", "SCSS", "JavaScript"],
       link: "https://todo-by-shyam.netlify.app/",
       github: "https://github.com/shyamg090/todo-react/"
     }, {
       id: 4,
       image: pokedex,
       name: "Pokedex",
-      desc: "Pokedex: React-based app showcases 1218 Pokémon with detailed info, built using React.js and SCSS",
+      skills: ["React.js", "SCSS", "JavaScript", "Poke API"],
+      desc: "Pokedex: React-based app showcases 1218 Pokémon with detailed info",
       link: "https://pokedex-by-shyam.netlify.app/",
       github: "https://github.com/shyamg090/Pokedex-React-project"
     },
@@ -51,7 +55,7 @@ export default function Projects() {
       </h3>
       {/* <div className='grid grid-col-1 md:grid-cols-2 flex-col  md:m-10 ' > */}
         {
-          myProject.map(({ id, image, name, desc, link, github }) => {
+          myProject.map(({ id, image, name, desc, link, github,skills }) => {
             return (
               <div key={id} className=' flex flex-col md:grid md:grid-cols-2 my-5 md:mx-20 shadow-md shadow-teal-300 rounded-lg  md:transition ease-in-out delay-150 mg:hover:-translate-y-1 duration-300 md:hover:shadow-green-400 ' >
                 <div className='flex flex-col items-center justify-center'>
@@ -62,6 +66,13 @@ export default function Projects() {
                 </div>
                 <div className='flex flex-col items-center justify-around' >
                   <p className='p-3 text-md md:text-xl' >{desc}</p>
+                  <div className='w-full p-3 flex items-center justify-evenly flex-wrap' >
+                    {
+                      skills.map((item) => {
+                        return <h3 className='m-2' key={id*2} ><span className='p-1 bg-cyan-700 rounded-lg text-white' >{item}</span></h3>
+                      })
+                    }
+                  </div>
                   <div className='flex items-center justify-evenly m-4 p-1 w-inherit' >
                     <a href={link} ><HiLink className='w-8 h-8 hover:text-teal-600 mx-10' /></a>
                     <a href={github}><PiGithubLogoFill className='w-8 h-8 hover:text-teal-600 mx-10' /></a>
